bfb8f072941ab64344c0e446c9bf4c5355b7cf88
Adnan7209 <mdadnan28713@gmail.com>
2024-03-05 17:31:56 +0530

    responsive landing page done

--- src/layouts/Layout1.tsx
+++ src/layouts/Layout1.tsx
@@ -1,4 +1,6 @@
+import Footer from "@/components/custom/Footer";
 import Header from "@/components/custom/Header";
+import Hero from "@/components/custom/Hero";
 import { ReactNode } from "react";
 
 type PropsType = {
@@ -9,7 +11,9 @@ const Layout1 = ({ children }: PropsType) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
+      <Hero/>
       <div className="container mx-auto flex-1 py-10">{children}</div>
+      <Footer/>
     </div>
   );
 };
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,14 +1,22 @@
 import Layout1 from "@/layouts/Layout1";
-import { Navigate, Route, Routes } from "react-router-dom"
+import HomePage from "@/pages/HomePage";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 const AppRoutes = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Layout1>Home Page</Layout1>} />
-            <Route path="/user-profile" element={<span>User Profile</span>} />
-            <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout1>
+            <HomePage />
+          </Layout1>
+        }
+      />
+      <Route path="/user-profile" element={<span>User Profile</span>} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 };
 
 export default AppRoutes;
