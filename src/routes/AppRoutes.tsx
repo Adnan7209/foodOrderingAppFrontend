3e69cadc716c62fbad5b0202a32d7a54d8145225
Adnan7209 <mdadnan28713@gmail.com>
2024-03-07 00:33:07 +0530

    protected user-profile route

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,3 +1,4 @@
+import ProtectedRoute from "@/auth/ProtectedRoute";
 import Layout1 from "@/layouts/Layout1";
 import AuthCallbackPage from "@/pages/AuthCallbackPage";
 import HomePage from "@/pages/HomePage";
@@ -16,14 +17,17 @@ const AppRoutes = () => {
         }
       />
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
-      <Route
-        path="/user-profile"
-        element={
-          <Layout1>
-            <UserProfilePage />
-          </Layout1>
-        }
-      />
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/user-profile"
+          element={
+            <Layout1>
+              <UserProfilePage />
+            </Layout1>
+          }
+        />
+      </Route>
+
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
