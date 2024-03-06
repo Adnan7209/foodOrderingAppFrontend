6a27427f5287e6b97d77c3807f6b6166a7ea7fa4
Adnan7209 <mdadnan28713@gmail.com>
2024-03-06 22:42:24 +0530

    get-user-api done

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,11 +1,17 @@
-import { useUpdateMyUser } from "@/api/MyUserApi"
+import { useGetMyUser, useUpdateMyUser } from "@/api/MyUserApi"
 import UserProfileForm from "@/forms/userProfileForms/UserProfileForm"
 
 
 const UserProfilePage = () => {
-    const {updateUser,isLoading} = useUpdateMyUser();
+    const {currentUser, isLoading:isGetLoading} = useGetMyUser();
+    const {updateUser,isLoading:isUpdateLoading} = useUpdateMyUser();
+
+    if(isGetLoading){
+        return <span className=" animate-pulse text-5xl ">Loading...</span>
+    }
+
   return (
-    <UserProfileForm onSave={updateUser} isLoading={isLoading} />
+    <UserProfileForm onSave={updateUser} isLoading={isUpdateLoading} />
   )
 }
 
