41c1134ec377198924306b2c14f118a7cb016628
Adnan7209 <mdadnan28713@gmail.com>
2024-03-13 16:11:50 +0530

    added pagination in searchPage

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,4 +1,5 @@
 import { useSearchRestaurants } from "@/api/RestaurantApi";
+import PaginationSelector from "@/components/custom/PaginationSelector";
 import SearchBar, { SearchForm } from "@/components/custom/SearchBar";
 import SearchResultCard from "@/components/custom/SearchResultCard";
 import SearchResultInfo from "@/components/custom/SearchResultInfo";
@@ -7,20 +8,30 @@ import { useParams } from "react-router";
 
 export type SearchState = {
   searchQuery: string;
+  page: number;
 };
 
 const SearchPage = () => {
   const { city } = useParams();
   const [searchState, setSearchState] = useState<SearchState>({
     searchQuery: "",
+    page: 1,
   });
 
   const { results, isLoading } = useSearchRestaurants(searchState, city);
+
+  const setPage = (page: number) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      page,
+    }));
+  };
 
   const setSearchQuery = (searchFormData: SearchForm) => {
     setSearchState((prevState) => ({
       ...prevState,
       searchQuery: searchFormData.searchQuery,
+      page:1,
     }));
   };
 
@@ -28,6 +39,7 @@ const SearchPage = () => {
     setSearchState((prevState) => ({
       ...prevState,
       searchQuery: "",
+      page:1
     }));
   };
 
@@ -54,6 +66,11 @@ const SearchPage = () => {
         {results.data.map((restaurant) => (
           <SearchResultCard restaurant={restaurant} />
         ))}
+        <PaginationSelector
+          page={results.pagination.page}
+          pages={results.pagination.pages}
+          onPageChange={setPage}
+        />
       </div>
     </div>
   );
