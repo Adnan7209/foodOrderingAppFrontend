43494f5ca44ed541f0171dfaac0b06797ecb7d01
Adnan7209 <mdadnan28713@gmail.com>
2024-03-17 15:00:11 +0530

    get customer order request done

--- src/api/OrderApi.tsx
+++ src/api/OrderApi.tsx
@@ -1,5 +1,6 @@
+import { OrderType } from "@/types/OrderTypes";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
@@ -58,4 +59,29 @@ export const useCreateCheckoutSession = () => {
   }
 
   return { createCheckoutSession, isLoading };
+};
+
+export const useGetMyOrders = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const getMyOrdersRequest = async (): Promise<OrderType> => {
+    const accessToken = await getAccessTokenSilently();
+    const response = await fetch(`${API_BASE_URL}/api/order`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Failed to get orders");
+    }
+
+    return response.json();
+  };
+
+  const { data: orders, isLoading } = useQuery(
+    "fetchMyOrders",
+    getMyOrdersRequest
+  );
+
+  return { orders, isLoading };
 };
---
+++ src/types/OrderTypes.tsx
@@ -0,0 +1,30 @@
+import { RestaurantType } from "./RestaurantTypes";
+import { User } from "./UserTypes";
+
+export type OrderStatus =
+  | "placed"
+  | "paid"
+  | "inProgress"
+  | "outForDelivery"
+  | "delivered";
+
+export type OrderType = {
+  _id: string;
+  restaurant: RestaurantType;
+  user: User;
+  cartItems: {
+    menuItemId: string;
+    name: string;
+    quantity: string;
+  }[];
+  deliveryDetails: {
+    name: string;
+    addressLine1: string;
+    city: string;
+    email: string;
+  };
+  totalAmount: number;
+  status: OrderStatus;
+  createdAt: string;
+  restaurantId: string;
+};
